ed3b26a7017bfaf5fe79ff2fcb29eaaf9df07a7b
Tudor Andrei Pop <tudor_fb@yahoo.com>
2021-08-09 09:50:53 +0300

    start script

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello our class !!");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
